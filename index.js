14efdf723ea33342bdca0fc27de0298eb8b4fb5e
Castro380 <mateuso@iesb.edu.br>
2024-03-06 22:25:22 -0300

    post

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, res){
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', function (req, res) {
     res.send('Rota Principal')
 })
-app.get('/hello', function(req, res){
+
+app.get('/hello', function (req, res) {
     res.send('Chegou a resposta')
 })
-app.get('/nome', function(req, res){
+
+app.get('/nome', function (req, res) {
     res.send('Mateus Castro')
 })
 
+app.post('/lista1/ex1', function (req, res){
 
-app.listen(3000, function(){
-    console.log('Server up port 3000')
+    const total = Number(req.body.total)
+    const brancos = Number(req.body.brancos)
+    const nulos = Number(req.body.nulos)
+    const validos = Number(req.body.validos)
+
+    const soma = brancos + nulos + validos
+    let retorno = {}
+    if (soma > total) {
+
+        retorno = {
+            codigo: 'Soma_Eleitores',
+            mensagem: "Erro: A soma dos votos não pode passar o total de eleitores"
+        }
+
+        res.status(400).json(retorno)
+
+
+    } else {
+        const percBranco = brancos / total * 100
+        const percNulos = nulos / total * 100
+        const percValidos = validos / total * 100
+
+
+        retorno = { percBranco, percNulos, percValidos }
+
+        res.status(200).json(retorno)
+    }
 })
+
+    app.listen(3000, function () {
+        console.log('Server up port 3000')
+    })
